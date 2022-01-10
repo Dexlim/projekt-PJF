f73d318892b0bf49af0abe28e17bd2c1548785a8
Wojciech Zalewski <zalewskiwoj8@gmail.com>
2022-01-10 17:36:07 +0100

    Discord API base

--- main.py
+++ main.py
@@ -2,16 +2,21 @@ import discord
 
 client = discord.Client()
 
+
 @client.event
 async def on_ready():
-  print("Logged in as {0.user}".format(client))
+    print("Logged in as {0.user}".format(client))
+
 
 @client.event
 async def on_message(message):
-  if message.author == client.user:
-    return
-  if message.content.startswith("$help"):
-    await message.channel.send("Available commands:")
+    if message.author == client.user:
+        return
+    if message.content.startswith("$help"):
+        msg = message.content.split("$help",1)[1]
+        if msg.startswith(" "):
+            msg = msg[1:]
+        await message.channel.send("Available commands:", reference=message)
 
 
 client.run('OTMwMDgyMzAwMDc3NjcwNDYy.YdwspA.GWhFT0o997OFLFgSPrmoghsWjvw')
