594c2d12ced4c9695495d48abfe13bd05d6b395b
Wojciech Zalewski <zalewskiwoj8@gmail.com>
2022-01-11 01:11:48 +0100

    Add embed creator

--- embed.py
+++ embed.py
@@ -1,17 +1,16 @@
 import discord
 
-def createEmbed(title,bot,offprice,keyshprice,gameimage,page,pagemax):
-  embed = discord.Embed(title="GAME_TITLE", color=0xeb5ca0)
-  embed.set_author(name="BOT_NAME", icon_url="https://i.pinimg.com/originals/b2/20/9f/b2209f5436079a03492468a177dccda3.jpg")
 
-  embed.set_thumbnail(url="https://i.pinimg.com/originals/b2/20/9f/b2209f5436079a03492468a177dccda3.jpg")
+def createEmbed(title, bot, offprice, keyshprice, gameimage, page, pagemax):
+    embed = discord.Embed(title=title, color=0xeb5ca0)
+    embed.set_author(name=bot, icon_url="https://i.pinimg.com/originals/b2/20/9f/b2209f5436079a03492468a177dccda3.jpg")
 
-  embed.add_field(name="Official stores: ", value="OFFICIAL_PRICE", inline=True)
-  embed.add_field(name="Keyshops: ", value="KEYSHOP_PRICE", inline=True)
+    embed.set_thumbnail(url="https://i.pinimg.com/originals/b2/20/9f/b2209f5436079a03492468a177dccda3.jpg")
 
-  embed.set_image(url="GAME_IMAGE_URL")
+    embed.add_field(name="Official stores: ", value=offprice, inline=True)
+    embed.add_field(name="Keyshops: ", value=keyshprice, inline=True)
 
-  embed.set_footer(text="Page 1/5")
-  return embed
+    embed.set_image(url=gameimage)
 
-
+    embed.set_footer(text="Page" + str(page) + "/" + str(pagemax))
+    return embed
